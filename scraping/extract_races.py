9929f9d27bf669e015c28dd6807cc7212b844264
João Vítor Fernandes <up201806724@up.pt>
2021-11-09 15:50:25 +0000

    Improvement to extracting script

--- scraping/extract_races.py
+++ scraping/extract_races.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
@@ -11,31 +12,55 @@ raceId = 0
 
 data = {'raceId' : [], 'paragraph' : []}
 
+
+def cleanParagraph(s):
+  if(s.name == 'p'):
+    for sup in s.findAll('sup'):
+      sup.decompose()
+    for a in s.findAll('a'):
+      a.replaceWithChildren()
+  else:
+    return ''
+
+  text = s.text.replace('\xa0','') 
+  text = text.replace('\n','') 
+
+  return text
+
 for url in urls:
   raceId += 1
   html = urlopen(url)
   bs = BeautifulSoup(html,'html.parser')
-  span = bs.find("span",{'id': 'Race'})
+  first_p = bs.p
+  span = bs.find('span', {'id': re.compile('Race.*')})
 
+  # first paragraphs
+  while(first_p is not None and not re.match('h[1-9]',first_p.name)):
+    if(first_p.name == 'p'):
+      text = cleanParagraph(first_p)
+      if text:
+        data['raceId'].append(raceId)
+        data['paragraph'].append(text)
 
+    first_p = first_p.find_next_sibling()
+
+  # race sections
   if span is not None:
-    parent = span.find_parent('h3')
+    parent = span.find_parent(re.compile('h[1-9]'))
     if parent is not None:
       s = parent.find_next_sibling('p')
 
-      while(s is not None and (s.name == 'p' or s.name == 'div')):
-        if(s.name == 'p'):
-          for sup in s.findAll('sup'):
-            sup.decompose()
-          for a in s.findAll('a'):
-            a.replaceWithChildren()
+      # paragraphs
+      while(s is not None and not re.match('h[1-9]',s.name)):
+        text = cleanParagraph(s)
 
-          text = s.text.replace('\n','') 
+
+        # not empty
+        if text:
           data['raceId'].append(raceId)
           data['paragraph'].append(text)
-
+        
         s = s.find_next_sibling()
-
 
 df = pd.DataFrame(data=data)
 df.to_csv('race_text.csv',index=False)
