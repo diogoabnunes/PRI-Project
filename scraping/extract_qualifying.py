c5b373db0cabb211a7fe92be86fdc042127594b0
João Vítor Fernandes <up201806724@up.pt>
2021-11-13 15:00:38 +0000

    Scraping qualifying report

--- scraping/extract_qualifying.py
+++ scraping/extract_qualifying.py
@@ -12,54 +12,32 @@ raceId = 0
 
 data = {'raceId' : [], 'paragraph' : []}
 
+#719, 735
 
 def cleanParagraph(s):
-  if(s.name == 'p'):
+  if(s.name == 'p' and not re.match('^[*].*$', s.text)):
     for sup in s.findAll('sup'):
       sup.decompose()
     for a in s.findAll('a'):
       a.replaceWithChildren()
+    for span in s.findAll('span'):
+      span.decompose()
   else:
     return ''
 
   text = s.text.replace('\xa0','') 
   text = text.replace('\n','') 
+
+  if(re.match('^  -',text)):
+      text = text[3:]
+
   return text
 
 for url in urls:
   raceId += 1
   html = urlopen(url)
   bs = BeautifulSoup(html,'html.parser')
-  first_p = bs.p
-  span = bs.find('span', {'id': re.compile('Race.*')})
-  qualifying = bs.find('span', {'id': 'Qualifying'})
-
-  # first paragraphs
-  while(first_p is not None and not re.match('h[1-9]',first_p.name)):
-    if(first_p.name == 'p' and first_p.find('span') is None):
-      text = cleanParagraph(first_p)
-      if text:
-        data['raceId'].append(raceId)
-        data['paragraph'].append(text)
-
-    first_p = first_p.find_next_sibling()
-
-  # race sections
-  if span is not None:
-    parent = span.find_parent(re.compile('h[1-9]'))
-    if parent is not None:
-      s = parent.find_next_sibling('p')
-
-      # paragraphs
-      while(s is not None and not re.match('h[1-9]',s.name)):
-        text = cleanParagraph(s)
-
-        # not empty
-        if text:
-          data['raceId'].append(raceId)
-          data['paragraph'].append(text)
-        
-        s = s.find_next_sibling()
+  qualifying = bs.find('span', {'id': re.compile('.*[qQ]ualifying$')})
 
   # qualiying section
   if qualifying is not None:
@@ -75,9 +53,8 @@ for url in urls:
         if text:
           data['raceId'].append(raceId)
           data['paragraph'].append(text)
-        
+
         s = s.find_next_sibling()
 
-
 df = pd.DataFrame(data=data)
-df.to_csv('race_text.csv',index=False)
+df.to_csv('qualifying_text.csv',index=False)
