f72c617e84c85ff2b89b5c30087df3986833d35c
João Vítor Fernandes <up201806724@up.pt>
2022-01-02 17:11:32 +0000

    fixing scraping from circuit with different wikipedia format

--- src/extract_circuits.py
+++ src/extract_circuits.py
@@ -37,6 +37,10 @@ for i in range(len(circuits)):
     html = urlopen(urls[i])
     bs = BeautifulSoup(html, 'html.parser')
     first_p = bs.p
+    # fix circuit 26 having a different format from the rest
+    if(first_p and circuitId == 26):
+      first_p = first_p.find_next('p')
+
     last = bs.find('span', {'id': 'See_also'})
 
     while (first_p is not None and first_p != last):
@@ -46,6 +50,7 @@ for i in range(len(circuits)):
                 data['circuitId'].append(circuitId)
                 data['text'].append(text) 
         first_p = first_p.find_next_sibling()
+
 
 df = pd.DataFrame(data=data)
 df.to_csv('../processed_data/circuits_text.csv', index=False)
