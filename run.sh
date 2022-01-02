4e6f00e48b8ad73352412195b90241f26d73946b
Diogo Nunes <up201808546@fe.up.pt>
2022-01-02 17:58:21 +0000

    for schema version, we have 5 datasets on Solr on the same core

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ curl -X POST -H 'Content-type:application/csv' --data-binary @C:\\Users\\'Diogo 
 curl -X POST -H 'Content-type:application/csv' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\documentos_resultado\\seasons_joined.csv localhost:8983/solr/races_schema/update?commit=true
 curl -X POST -H 'Content-type:application/csv' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\documentos_resultado\\drivers_joined.csv localhost:8983/solr/races_schema/update?commit=true
 curl -X POST -H 'Content-type:application/csv' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\documentos_resultado\\constructors_joined.csv localhost:8983/solr/races_schema/update?commit=true
-
+curl -X POST -H 'Content-type:application/csv' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\documentos_resultado\\circuits_joined.csv localhost:8983/solr/races_schema/update?commit=true
 
 # Schema with boosts
 curl -X POST -H 'Content-type:application/json' -d @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\schema\\schema.json http://localhost:8983/solr/races_boost/schema
