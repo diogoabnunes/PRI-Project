3e5fba9c3b26aca2855774c23bb1e15272993866
Diogo Nunes <up201808546@fe.up.pt>
2022-01-02 17:56:53 +0000

    merging all datasets

--- run.sh
+++ run.sh
@@ -6,9 +6,17 @@ docker exec 050b84d33977 bin/solr create_core -c races_default
 docker exec 050b84d33977 bin/solr create_core -c races_schema
 docker exec 050b84d33977 bin/solr create_core -c races_boost
 
-curl -X POST -H 'Content-type:application/json' -d @C:\Users\'Diogo Nunes'\'OneDrive - Universidade do Porto'\FEUP\PRI\Formula1\schema\schema.json http://localhost:8983/solr/races_schema/schema
-curl -X POST -H 'Content-type:application/json' -d @C:\Users\'Diogo Nunes'\'OneDrive - Universidade do Porto'\FEUP\PRI\Formula1\schema\schema.json http://localhost:8983/solr/races_boost/schema
+# Default (without schema)
+curl -X POST -H 'Content-type:application/csv' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\documentos_resultado\\races_joined.csv localhost:8983/solr/races_default/update?commit=true
 
-curl -X POST -H 'Content-type:application/csv' --data-binary @C:\Users\'Diogo Nunes'\'OneDrive - Universidade do Porto'\FEUP\PRI\Formula1\documentos_resultado\races_joined.csv localhost:8983/solr/races_default/update?commit=true
-curl -X POST -H 'Content-type:application/csv' --data-binary @C:\Users\'Diogo Nunes'\'OneDrive - Universidade do Porto'\FEUP\PRI\Formula1\documentos_resultado\races_joined.csv localhost:8983/solr/races_schema/update?commit=true
-curl -X POST -H 'Content-type:application/csv' --data-binary @C:\Users\'Diogo Nunes'\'OneDrive - Universidade do Porto'\FEUP\PRI\Formula1\documentos_resultado\races_joined.csv localhost:8983/solr/races_boost/update?commit=true
+# Schema
+curl -X POST -H 'Content-type:application/json' -d @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\schema\\schema.json http://localhost:8983/solr/races_schema/schema
+curl -X POST -H 'Content-type:application/csv' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\documentos_resultado\\races_joined.csv localhost:8983/solr/races_schema/update?commit=true
+curl -X POST -H 'Content-type:application/csv' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\documentos_resultado\\seasons_joined.csv localhost:8983/solr/races_schema/update?commit=true
+curl -X POST -H 'Content-type:application/csv' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\documentos_resultado\\drivers_joined.csv localhost:8983/solr/races_schema/update?commit=true
+curl -X POST -H 'Content-type:application/csv' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\documentos_resultado\\constructors_joined.csv localhost:8983/solr/races_schema/update?commit=true
+
+
+# Schema with boosts
+curl -X POST -H 'Content-type:application/json' -d @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\schema\\schema.json http://localhost:8983/solr/races_boost/schema
+curl -X POST -H 'Content-type:application/csv' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\documentos_resultado\\races_joined.csv localhost:8983/solr/races_boost/update?commit=true
--- solr_commands.md
+++ solr_commands.md
@@ -7,7 +7,7 @@
 - docker exec 050b84d33977 bin/solr create_core -c races
 
 ## Populating collection
-- **API**: curl -X POST -H 'Content-type:application/csv' --data-binary @C:\Users\'Diogo Nunes'\'OneDrive - Universidade do Porto'\FEUP\PRI\Formula1\documentos_resultado\races_joined.csv localhost:8983/solr/races/update?commit=true
+- **API**: curl -X POST -H 'Content-type:application/csv' --data-binary @C:\Users\'Diogo Nunes'\'OneDrive - Universidade do Porto'\FEUP\PRI\Formula1\documentos_resultado\races_joined.csv localhost:8983/solr/races_schema/update?commit=true
 
 ## Schema
 - **API**: curl -X POST -H 'Content-type:application/json' -d @C:\Users\'Diogo Nunes'\'OneDrive - Universidade do Porto'\FEUP\PRI\Formula1\schema\schema.json http://localhost:8983/solr/races/schema
