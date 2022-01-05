5c2360c5fad1341b709c033ae25324ac395d0a2f
João Vítor Fernandes <up201806724@up.pt>
2022-01-05 19:08:58 +0000

    fixed extract_drivers.py

--- src/extract_drivers.py
+++ src/extract_drivers.py
@@ -17,7 +17,6 @@ def cleanParagraph(s):
   text = s.text.replace('\xa0','') 
   text = text.replace('\n','') 
   text = text.replace('(key)','')
-  text = text.replace('.', '')
   text = text.replace('Season still in progress.', '')
   text = text.replace('* Season still in progress.','')
   text = text.replace('† Not eligible for points', '')
@@ -26,10 +25,15 @@ def cleanParagraph(s):
   text = text.replace('(Races in bold indicate pole position; races in italics indicate fastest lap)', '')
   text = text.strip()
 
-  if text.endswith(':'): text = ''
-  elif text.startswith('(') and text.endswith(')'): text = ''
-  elif text.startswith('*') or text.startswith('†') or text.startswith('‡'): text = ''
-  elif not text.endswith('.'): text = ''
+  if text.endswith(':'):
+    text = ''
+  elif text.startswith('(') and text.endswith(')'):
+    text = ''
+  elif text.startswith('*') or text.startswith('†') or text.startswith('‡'):
+    text = ''
+  elif not text.endswith('.'):
+    text = ''
+
 
   return text
 
@@ -44,16 +48,23 @@ for i in range(len(drivers)):
     driverId = driverIds[i]
     print(f'DriverID: {driverId}')
     url = urls[i]
-
-    html = urlopen(urls[i])
+    html = urlopen(url)
     bs = BeautifulSoup(html, 'html.parser')
     first_p = bs.p
+    pclass = None
+
+    # fix empty paragraph recently added to some driver's wikipedia pages
+    if first_p and first_p.has_attr('class'):
+      pclass =  first_p['class'][0]
+      if(pclass == 'mw-empty-elt'):
+        first_p = first_p.find_next('p')
+
     last = bs.find('span', {'id': 'See_also'})
 
     while (first_p is not None and first_p != last):
         if (first_p.name == 'p'):
             text = cleanParagraph(first_p)
-            if text and text != '(key)':
+            if text:
                 data['driverId'].append(driverId)
                 data['text'].append(text) 
         first_p = first_p.find_next_sibling()
