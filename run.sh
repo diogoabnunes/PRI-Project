be67801e3b016f62d822da31b7920b254000e640
Diogo Nunes <up201808546@fe.up.pt>
2022-01-16 14:48:12 +0000

    run.sh works for everyone

--- run.sh
+++ run.sh
@@ -5,19 +5,19 @@ docker exec 8d4b10ab44f1 bin/solr create_core -c races_default
 docker exec 8d4b10ab44f1 bin/solr create_core -c races_schema
 
 # Default (without schema)
-curl -X POST -H 'Content-type:application/json' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\data\\final_json\\races.json localhost:8983/solr/races_default/update?commit=true
-curl -X POST -H 'Content-type:application/json' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\data\\final_json\\seasons.json localhost:8983/solr/races_default/update?commit=true
-curl -X POST -H 'Content-type:application/json' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\data\\final_json\\drivers.json localhost:8983/solr/races_default/update?commit=true
-curl -X POST -H 'Content-type:application/json' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\data\\final_json\\constructors.json localhost:8983/solr/races_default/update?commit=true
-curl -X POST -H 'Content-type:application/json' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\data\\final_json\\circuits.json localhost:8983/solr/races_default/update?commit=true
-curl -X POST -H 'Content-type:application/json' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\data\\final_json\\pages.json localhost:8983/solr/races_default/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/races.json localhost:8983/solr/races_default/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/seasons.json localhost:8983/solr/races_default/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/drivers.json localhost:8983/solr/races_default/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/constructors.json localhost:8983/solr/races_default/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/circuits.json localhost:8983/solr/races_default/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/pages.json localhost:8983/solr/races_default/update?commit=true
 
 # Schema
-curl -X POST -H 'Content-type:application/json' -d @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\data\\schema\\schema.json http://localhost:8983/solr/races_schema/schema
+curl -X POST -H 'Content-type:application/json' -d @./data/schema/schema.json http://localhost:8983/solr/races_schema/schema
 
-curl -X POST -H 'Content-type:application/json' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\data\\final_json\\races.json localhost:8983/solr/races_schema/update?commit=true
-curl -X POST -H 'Content-type:application/json' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\data\\final_json\\seasons.json localhost:8983/solr/races_schema/update?commit=true
-curl -X POST -H 'Content-type:application/json' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\data\\final_json\\drivers.json localhost:8983/solr/races_schema/update?commit=true
-curl -X POST -H 'Content-type:application/json' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\data\\final_json\\constructors.json localhost:8983/solr/races_schema/update?commit=true
-curl -X POST -H 'Content-type:application/json' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\data\\final_json\\circuits.json localhost:8983/solr/races_schema/update?commit=true
-curl -X POST -H 'Content-type:application/json' --data-binary @C:\\Users\\'Diogo Nunes'\\'OneDrive - Universidade do Porto'\\FEUP\\PRI\\Formula1\\data\\final_json\\pages.json localhost:8983/solr/races_schema/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/races.json localhost:8983/solr/races_schema/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/seasons.json localhost:8983/solr/races_schema/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/drivers.json localhost:8983/solr/races_schema/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/constructors.json localhost:8983/solr/races_schema/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/circuits.json localhost:8983/solr/races_schema/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/pages.json localhost:8983/solr/races_schema/update?commit=true
