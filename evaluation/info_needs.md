9d9befc38c6eaf646178f3b24ea03e1e6a754635
Diogo Nunes <up201808546@fe.up.pt>
2022-01-16 20:28:21 +0000

    frontend example

--- evaluation/info_needs.md
+++ evaluation/info_needs.md
@@ -1,41 +1,13 @@
 # Information Needs
 
-## Races (Qualifying + Race)
+## Races
 - Youngest driver to win a race or a championship (at their time)
-- Incidents involving a certain driver
-- Races where a certain driver made overtakes
+- Incidents / Crashes / Contacts involving a certain driver
+- Overtakes of a certain driver
 - (Races where a certain driver started from 1st place)
 
 ## Drivers
 - Teammates of a certain driver
-- Teams that a certain driver has represented / is representing
-- Drivers that, on their road to F1, won F2/F3 championships
-- Drivers that won on their home Grand Prix
-- Drivers nicknames (ex. Raikkonen is "The Iceman")
-- Drivers from a certain nationality
-- Check if a certain driver already won a championship
 
-## Constructors
-- Teams from a certain country
-- Teams engine suppliers throughout the years (ex. Red Bull is constantly changing)
-- Drivers from a certain team throughout the years
-- Check if a certain team already won a championship
-when did x team won a championship
-
-## Circuits
-- Circuits from a certain country
-- Circuits from a certain city
-- Countries that hosted races on different cities
-- Motorsport categories that got races on a certain circuit
-- Longest and shortest circuits (in terms of distance)
-
-## Seasons
-- Drivers that most fighted for the championship (ex. this year, Max and Lewis)
-- New circuits on an F1 season
-- Circuits that "fall" off the F1 calendar
-- Drivers rookie seasons (their debut on F1)
-- Constructors debut
-- Season champion (driver and constructors)
-- Record for most (consecutive) Drivers/Constructors titles
-- Leaders change during the season
-- Race wins from a driver in 1 season
+# Pages
+- Fastest Lap Award description and winners
--- frontend/pri-app/src/components/Results.js
+++ frontend/pri-app/src/components/Results.js
@@ -2,9 +2,12 @@ import {Card} from "react-bootstrap"
 
 export function Results() {
     const items = [];
-    for(var i = 0; i < 5; i++) {
-        items.push(card());        
-    }
+    items.push(card1());   
+    items.push(card2());        
+    items.push(card3());        
+    items.push(card4());        
+    items.push(card5());        
+
     return(
         <div>
             {items}
@@ -12,17 +15,91 @@ export function Results() {
     )
 }
 
-function card(){
+function card1(){
     return(
         <Card className="m-4">
-            <Card.Header>Race</Card.Header>
+            <Card.Header>Race: 2020 Portuguese Grand Prix</Card.Header>
             <Card.Body>
-            <blockquote className="blockquote mb-0">
+            <blockquote className="blockquote mb-0 d-flex">
                 <p>
                 {' '}
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-                erat a ante.{' '}
-                </p>
+                The 2020 Portuguese Grand Prix (officially known as the Formula 1 Heineken Grande Prémio de Portugal 2020) was a Formula One motor race that was held on 25 October 2020 at the Algarve International Circuit in Portimão, Portugal.
+                It was the first Portuguese Grand Prix held since 1996 and the first time held at the Algarve International Circuit.
+                The race was the twelfth round of the 2020 Formula One World Championship.
+                Hamilton's victory put him ahead of Michael Schumacher for most victories in Formula One with 92.{' '}
+                <a href="/">See more</a></p>
+            </blockquote>
+            </Card.Body>
+        </Card>
+    )
+}
+
+function card2(){
+    return(
+        <Card className="m-4">
+            <Card.Header>Driver: Lewis Hamilton</Card.Header>
+            <Card.Body>
+            <blockquote className="blockquote mb-0 d-flex">
+                <p>
+                {' '}
+                Sir Lewis Carl Davidson Hamilton MBE HonFREng (born 7 January 1985) is a British racing driver.
+                He currently competes in Formula One for Mercedes, having previously driven for McLaren from 2007 to 2012.
+                In Formula One, Hamilton has won a joint-record seven World Drivers' Championship titles (tied with Michael Schumacher), and holds the records for the most wins (103), pole positions (103), and podium finishes (182), among others.{' '}
+                <a href="/">See more</a></p>
+            </blockquote>
+            </Card.Body>
+        </Card>
+    )
+}
+
+function card3(){
+    return(
+        <Card className="m-4">
+            <Card.Header>Constructor: Ferrari</Card.Header>
+            <Card.Body>
+            <blockquote className="blockquote mb-0 d-flex">
+                <p>
+                {' '}
+                Scuderia Ferrari S.p.A. (Italian: [skudeˈriːa ferˈraːri]) is the racing division of luxury Italian auto manufacturer Ferrari and the racing team that competes in Formula One racing.
+                The team is also nicknamed "The Prancing Horse", in reference to their logo.
+                It is the oldest surviving and most successful Formula One team, having competed in every world championship since the 1950 Formula One season.{' '}
+                <a href="/">See more</a></p>
+            </blockquote>
+            </Card.Body>
+        </Card>
+    )
+}
+
+function card4(){
+    return(
+        <Card className="m-4">
+            <Card.Header>Circuit: Portimao</Card.Header>
+            <Card.Body>
+            <blockquote className="blockquote mb-0 d-flex">
+                <p>
+                {' '}
+                The Algarve International Circuit (Portuguese: Autódromo Internacional do Algarve), commonly referred to as Portimão Circuit, is a 4.653 km (2.891 mi) race circuit located in Portimão, Algarve region, in Portugal.
+                The development includes a karting track, off-road track, technology park, five-star hotel, sports complex and apartments.
+                The circuit was designed by Ricardo Pina, Arquitectos.
+                The Construction was finished in October 2008 and the circuit was homologated by both the FIM on 11 October 2008 and the FIA two days later. The total cost was €195 million (approximately $250 million).{' '}
+                <a href="/">See more</a></p>
+            </blockquote>
+            </Card.Body>
+        </Card>
+    )
+}
+
+function card5(){
+    return(
+        <Card className="m-4">
+            <Card.Header>Page: DHL Fastest Lap Award</Card.Header>
+            <Card.Body>
+            <blockquote className="blockquote mb-0 d-flex">
+                <p>
+                {' '}
+                The DHL Fastest Lap Award is given annually by the courier, Formula One global partner and logistics provider DHL "to recognise the driver who most consistently demonstrates pure speed, with the fastest lap at the highest number of races each season", and to reward the winning driver for "characteristics such as excellent performance, passion, can-do attitude, reliability and precision".
+                {' '}
+                <a href="/">See more</a></p>
             </blockquote>
             </Card.Body>
         </Card>
