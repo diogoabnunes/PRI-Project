8d7fa66452f41018213d4cabea6b926ff1a3b73d
Diogo Nunes <up201808546@fe.up.pt>
2022-01-16 17:38:21 +0000

    core configurations

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
 docker exec 8d4b10ab44f1 bin/solr delete -c races_default
 docker exec 8d4b10ab44f1 bin/solr delete -c races_schema
+docker exec 8d4b10ab44f1 bin/solr delete -c races_boost
 
 docker exec 8d4b10ab44f1 bin/solr create_core -c races_default
 docker exec 8d4b10ab44f1 bin/solr create_core -c races_schema
+docker exec 8d4b10ab44f1 bin/solr create_core -c races_boost
 
 # Default (without schema)
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/schema/configDefault.json http://localhost:8983/solr/races_default/config
+
 curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/races.json localhost:8983/solr/races_default/update?commit=true
 curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/seasons.json localhost:8983/solr/races_default/update?commit=true
 curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/drivers.json localhost:8983/solr/races_default/update?commit=true
@@ -14,6 +18,7 @@ curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json
 
 # Schema
 curl -X POST -H 'Content-type:application/json' -d @./data/schema/schema.json http://localhost:8983/solr/races_schema/schema
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/schema/configSchema.json http://localhost:8983/solr/races_schema/config
 
 curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/races.json localhost:8983/solr/races_schema/update?commit=true
 curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/seasons.json localhost:8983/solr/races_schema/update?commit=true
@@ -21,3 +26,14 @@ curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json
 curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/constructors.json localhost:8983/solr/races_schema/update?commit=true
 curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/circuits.json localhost:8983/solr/races_schema/update?commit=true
 curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/pages.json localhost:8983/solr/races_schema/update?commit=true
+
+# Schema with boosts
+curl -X POST -H 'Content-type:application/json' -d @./data/schema/schema.json http://localhost:8983/solr/races_boost/schema
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/schema/configBoost.json http://localhost:8983/solr/races_boost/config
+
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/races.json localhost:8983/solr/races_boost/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/seasons.json localhost:8983/solr/races_boost/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/drivers.json localhost:8983/solr/races_boost/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/constructors.json localhost:8983/solr/races_boost/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/circuits.json localhost:8983/solr/races_boost/update?commit=true
+curl -X POST -H 'Content-type:application/json' --data-binary @./data/final_json/pages.json localhost:8983/solr/races_boost/update?commit=true
